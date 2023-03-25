3eff0ed15a00159ef7332fa3423d11c899859219
rdeager14 <rdeager@miners.utep.edu>
2023-03-24 21:52:01 -0600

    Update code_13.py

--- code_13.py
+++ code_13.py
@@ -1,3 +1,16 @@
+def get_merge_sorted_list(unsorted_list):
+    if len(unsorted_list)==1:
+        return unsorted_list
+    midland = (int(len(unsorted_list))//2)
+
+    east_side = unsorted_list[:midland]
+    west_side = unsorted_list[midland:]
+
+    half_east = get_merge_sorted_list(east_side)
+    half_west = get_merge_sorted_list(west_side)
+
+    return time_to_merge(half_east,half_west)
+
 def time_to_merge(east, west):
     i = j = 0
     merged_city = []
