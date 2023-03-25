922e812592b081eaae41a76941dff4b017c4188b
rdeager14 <rdeager@miners.utep.edu>
2023-03-24 21:20:30 -0600

    Update code_13.py

--- code_13.py
+++ code_13.py
@@ -1,5 +1,19 @@
-def get_merge_sorted_list(unsorted_list):
-    pass #Remove this line of code and implement your merge sort algorithm
+def time_to_merge(west,east):
+    return
 
+def get_merge_sorted_list(unsorted_list):
+    if len(unsorted_list)==1:
+        return unsorted_list
+    midland = (int(len(unsorted_list))//2)
+
+    east_side = unsorted_list[:midland]
+    west_side = unsorted_list[midland:]
+
+    half_east = get_merge_sorted_list(east_side)
+    half_west = get_merge_sorted_list(west_side)
+
+    return time_to_merge(half_east,half_west)
+
+    
 if __name__ == "__main__":
-    pass #Remove this line and program your function calls to locally test the functionallity of merge sort
+    pass
