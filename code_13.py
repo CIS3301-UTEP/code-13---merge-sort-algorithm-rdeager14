90a5dadb1ba0e4157eaa8ecb1ea071cb14da258e
rdeager14 <rdeager@miners.utep.edu>
2023-03-24 21:27:36 -0600

    Update code_13.py

--- code_13.py
+++ code_13.py
@@ -1,5 +1,4 @@
-def time_to_merge(west,east):
-    return
+
 
 def get_merge_sorted_list(unsorted_list):
     if len(unsorted_list)==1:
@@ -14,6 +13,40 @@ def get_merge_sorted_list(unsorted_list):
 
     return time_to_merge(half_east,half_west)
 
+def time_to_merge(east, west):
+    i = j = 0
+    merged_city = []
+
+    while i < len(east) and j < len(west):
+        if east[i] < west[j]:
+            merged_city.append(east[i])
+            i += 1
+        else: 
+            merged_city.append(west[j])
+            j += 1
+
+    while i < len(east):
+        merged_city.append(east[i])
+        i += 1
+
+    while j < len(west):
+        merged_city.append(west[j])
+        j += 1
+    
+    return merged_city
+
+
+
     
 if __name__ == "__main__":
-    pass
+    import random
+    numbers = 15
+    unordered = []
+for i in range(numbers):
+    unordered.append(random.randint(1,15))
+print("\nunordered\n")
+print(unordered)
+
+ordered = get_merge_sorted_list(unordered)
+print("\norderedn\n")
+print(ordered)
